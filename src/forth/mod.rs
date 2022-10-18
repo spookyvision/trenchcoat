aba30d31372b5f7f7d34fcb3b79d523837d4c4ab
Anatol Ulrich <anatol@versteht.es>
2022-10-18 15:00:49 +0200

    remove dewaka/forth-rs

--- src/forth/mod.rs
+++ src/forth/mod.rs
@@ -1,11 +1,6 @@
 pub mod bytecode;
 pub mod env;
-pub mod inter;
-mod ops;
+pub mod runtime;
 
 #[cfg(test)]
 pub mod util;
-
-fn valid_forth_name(name: &str) -> bool {
-    name.parse::<i32>().is_err()
-}
