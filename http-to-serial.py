215ab6a0a76623072b009373adcefe954027579d
Anatol Ulrich <anatol@versteht.es>
2022-10-31 20:52:47 +0100

    unfuxored webserver

--- http-to-serial.py
+++ http-to-serial.py
@@ -12,7 +12,7 @@ import logging
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
-        self.send_header("Access-Control-Allow-Origin:", "*")
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Content-type", "text/plain")
         self.end_headers()
 
